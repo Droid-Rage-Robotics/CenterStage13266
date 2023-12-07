30a4ca1e6cce676313979598730176748921bc58
JaxsB13266 <jackiebecerril9@gmail.com>
2023-12-07 13:07:39 -0600

    commit rr

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/SampleMecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/SampleMecanumDrive.java
@@ -51,7 +51,7 @@ import java.util.List;
  * Simple mecanum drive hardware implementation for REV hardware.
  */
 @Config
-public class SampleMecanumDrive extends MecanumDrive {.
+public class SampleMecanumDrive extends MecanumDrive {
     public static PIDCoefficients TRANSLATIONAL_PID = new PIDCoefficients(0, 0, 0);
     public static PIDCoefficients HEADING_PID = new PIDCoefficients(0, 0, 0);
 
